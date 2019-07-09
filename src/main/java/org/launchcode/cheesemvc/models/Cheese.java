535f71e13dc02851c94577905081c1740ed8ac1e
MartynovAV <mcae@yandex.ru>
2019-07-09 19:47:13 +0300

    before DAO

--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -1,9 +1,15 @@
 package org.launchcode.cheesemvc.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class Cheese {
+    @Id
+    @GeneratedValue
+    private int id;
+
     @NotNull
     @Size(min = 3,max=15,message ="Size should be between 3 and 15")
     private String name;
@@ -12,25 +18,17 @@ public class Cheese {
     @Size(min = 1,message = "Description must not be empty")
     private String description;
 
-    private int cheeseId;
-    private static int nextId=1;
+    private CheeseType type;
 
     public Cheese(String name, String description) {
-        this();
         this.name = name;
         this.description = description;
     }
     public Cheese(){
-        cheeseId =nextId;
-        nextId++;
     }
 
-    public int getCheeseId() {
-        return cheeseId;
-    }
-
-    public void setCheeseId(int cheeseId) {
-        this.cheeseId = cheeseId;
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -47,5 +45,13 @@ public class Cheese {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public CheeseType getType() {
+        return type;
+    }
+
+    public void setType(CheeseType type) {
+        this.type = type;
     }
 }
---
+++ src/main/java/org/launchcode/cheesemvc/models/data/CheeseDao.java
@@ -0,0 +1,7 @@
+package org.launchcode.cheesemvc.models.data;
+
+import org.launchcode.cheesemvc.models.Cheese;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CheeseDao extends CrudRepository<Cheese,Integer> {
+}
